2d33f340f9bda31355fadcffff5b7615187b2fcf
Kevin Chen <k.chen52@gmail.com>
2020-03-26 14:43:37 -0700

    Initial setup

---
+++ app/src/main/java/dev/ktown/forecastmvvm/ui/settings/SettingsFragment.kt
@@ -0,0 +1,8 @@
+package dev.ktown.forecastmvvm.ui.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment: Fragment() {
+}
---
+++ app/src/main/java/dev/ktown/forecastmvvm/ui/weather/future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package dev.ktown.forecastmvvm.ui.weather.future.list
+
+import androidx.lifecycle.ViewModel
+
+class FutureListWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
