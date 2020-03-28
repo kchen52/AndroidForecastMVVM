67903ce3acec0955b90f9c883970b4866829074a
Kevin Chen <k.chen52@gmail.com>
2020-03-28 12:08:08 -0700

    Add test call to check that the call is working

--- app/src/main/java/dev/ktown/forecastmvvm/data/WeatherStackApiService.kt
+++ app/src/main/java/dev/ktown/forecastmvvm/data/WeatherStackApiService.kt
@@ -1,18 +1,53 @@
 package dev.ktown.forecastmvvm.data
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import dev.ktown.forecastmvvm.data.response.CurrentWeatherResponse
 import kotlinx.coroutines.Deferred
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 const val API_KEY = "5a490c5f89a3621ec06d10f20f4840f8"
+const val BASE_URL = "http://api.weatherstack.com/"
 
 // http://api.weatherstack.com/current?access_key=5a490c5f89a3621ec06d10f20f4840f8&query=New%20York&lang=en
 
 interface WeatherStackApiService {
-    @GET("current.json")
+    @GET("current")
     fun getCurrentWeather(
         @Query("query") location: String,
         @Query("lang") languageCode: String = "en"
     ): Deferred<CurrentWeatherResponse>
+
+    companion object {
+        operator fun invoke(): WeatherStackApiService {
+            val requestInterceptor = Interceptor { chain ->
+                val url = chain.request()
+                    .url()
+                    .newBuilder()
+                    .addQueryParameter("access_key", API_KEY)
+                    .build()
+                val request = chain.request()
+                    .newBuilder()
+                    .url(url)
+                    .build()
+                return@Interceptor chain.proceed(request)
+            }
+
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(requestInterceptor)
+                .build()
+
+            return Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl(BASE_URL)
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(WeatherStackApiService::class.java)
+        }
+    }
 }
--- app/src/main/java/dev/ktown/forecastmvvm/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/dev/ktown/forecastmvvm/ui/weather/current/CurrentWeatherFragment.kt
@@ -1,13 +1,18 @@
 package dev.ktown.forecastmvvm.ui.weather.current
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 
 import dev.ktown.forecastmvvm.R
+import dev.ktown.forecastmvvm.data.WeatherStackApiService
+import kotlinx.android.synthetic.main.current_weather_fragment.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class CurrentWeatherFragment : Fragment() {
 
@@ -27,8 +32,17 @@ class CurrentWeatherFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(CurrentWeatherViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this).get(CurrentWeatherViewModel::class.java)
+
+        val apiService = WeatherStackApiService()
+        GlobalScope.launch(Dispatchers.Main) {
+            try {
+                val currentWeatherResult = apiService.getCurrentWeather("London").await()
+                textView.text = currentWeatherResult.toString()
+            } catch (exception: Exception) {
+                exception.printStackTrace()
+            }
+        }
     }
 
 }
